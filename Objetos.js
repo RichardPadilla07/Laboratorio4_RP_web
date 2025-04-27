5aea5c5b7ee4cd143c132e8a341ca9e558b8bed5
RichardPadilla07 <richard.padilla@epn.edu.ec>
2025-04-26 20:32:35 -0500

    commit 3

--- Objetos.js
+++ Objetos.js
@@ -18,3 +18,53 @@ const miembro = {
 // Ver informacion
 console.log(miembro)
 console.log(miembro.sendMail())
+
+
+// Eliminar propiedades
+delete miembro.actividad
+console.log(miembro)
+
+// Desestructuración
+const {descripcion, email, roles, contactos} = miembro
+console.log(descripcion)
+console.log(email)
+console.log(roles)
+console.log(contactos)
+
+
+
+
+
+
+
+// Segundo objeto de amigos de algun miembro
+const amigos = {
+    status: true,
+    listaAmigos: ["Richard", "Lucho777", "Mela_2417"],
+    sendEmail(){
+        return `Send email to ${amigos.listaAmigos[0]}`
+    }
+}
+
+console.log(amigos.sendEmail())
+
+
+// Concatenar dos objetos
+const allInformation = {...miembro, ...amigos}
+console.log(allInformation)
+
+//Agregar propiedades
+miembro.hasPFP = true
+amigos.hasPFP = true
+
+//Congelar objeto
+Object.freeze(miembro)
+console.log(Object.isFrozen(miembro));
+miembro.hasPFP = false
+console.log(miembro);
+
+//Sellar objeto
+Object.seal(amigos)
+console.log(Object.isSealed(amigos))
+amigos.hasPFP = false
+console.log(amigos);
